f9cc0153a3e9ea3d889606a3af46b32ef73307b3
Sbeltran16 <sbeltran16@gmail.com>
2022-04-26 16:59:35 -0700

    add sneaker model for Buyers and update the detail template for users

--- main_app/models.py
+++ main_app/models.py
@@ -3,11 +3,35 @@ from django.urls import reverse
 
 # Create your models here.
 class Sneaker(models.Model):
-    name = models.CharField(max_length=100)
-    brand = models.CharField(max_length=100)
-    description = models.CharField(max_length=250)
-    price = models.IntegerField()
+  name = models.CharField(max_length=100)
+  brand = models.CharField(max_length=100)
+  description = models.CharField(max_length=250)
+  price = models.IntegerField('Retail Price')
 
 
-    def get_absolute_url(self):
-        return reverse('detail', kwargs={'sneaker_id': self.id})
+  def get_absolute_url(self):
+      return reverse('detail', kwargs={'sneaker_id': self.id})
+
+SIZES = (
+  ('11', 'Size 11'),
+  ('10', 'Size 10'),
+  ('9', 'Size 9'),
+  ('8','Size 8'),
+  ('7','Size 7')
+)
+
+class Buyer(models.Model):
+  name = models.CharField('Buyer Name', max_length=100)
+  value = models.IntegerField('Resell Price')
+  bought = models.DateField('Date Bought')
+  size = models.CharField(
+    'Size Bought',
+    max_length=2,
+    choices=SIZES,
+    default=SIZES[0][0]
+  )
+
+  sneaker = models.ForeignKey(Sneaker, on_delete=models.CASCADE)
+
+  def __str__(self):
+    return f"{self.get_size_display()} on {self.bought}"
