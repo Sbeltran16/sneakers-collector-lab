4bf6fdbbfa68afef3f444e0dece90ed60f988e8e
Sbeltran16 <sbeltran16@gmail.com>
2022-04-25 21:35:52 -0700

    CreateDb and add folders and files for sneakers collector

---
+++ main_app/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse #res.send in express
+from .models import Cat #importing our model
+
+
+
+def home(request):
+	return HttpResponse('<h1>Welcome to Sneaker Collector!</>')
+
+def about(request):
+	return render(request, 'about.html')
+
+def sneakers_index(request):
+	sneakers = Sneaker.objects.all() # using our model to get all the rows in our cat table in psql
+	return render(request, 'sneakers/index.html', {'sneakers': sneakers})
+
+def sneakers_detail(request, sneaker_id): # path('cats/<int:cat_id>/' <- this is where cat_id comes from
+	sneaker = Sneaker.objects.get(id=sneaker_id)
+	return render(request, 'sneakers/detail.html', {'sneaker': sneaker})
+
